e602b59487548f621bed8fce4a896b17f1292638
jdavis6632 <datboi6632@gmail.com>
2025-03-23 22:18:39 +0000

    Added Lab1 console app

---
+++ Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 1 
+        Console.WriteLine("Jaivian Davis IT-1050");
+
+        // Step 2
+        /*  Name: [Jaivian]
+        Title: [IT-1050 - Lab 1]. */
+
+        // Step 3
+        int coolestNumber = 73; 
+        string peakCodingLang = "Python"; 
+        double previousPrograms = 50.5;
+        bool codingExperience = True; 
+
+        // Step 4
+        Console.WriteLine("What's greater than 72?" 'coolestNumber');
+        Console.WriteLine("Scariest Animal" 'peakCodingLang');
+        Console.WriteLine(previousPrograms)
+        Console.WriteLine(codingExperience)
+
+        //Step 5
+        const string schoolName = "Tri-C";
+        Console.WriteLine(schoolName);
+        
+
+
+    }
+}
